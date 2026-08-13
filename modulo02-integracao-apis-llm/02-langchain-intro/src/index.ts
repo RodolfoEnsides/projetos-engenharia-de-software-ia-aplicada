939631fceb9efda91ac2f89b465cd9222b66405f
rodolfo.ensdies <rensides@hotmail.com>
2026-08-13 19:25:39 -0300

    feat/initial-commit-cls-02

---
+++ modulo02-integracao-apis-llm/02-langchain-intro/src/index.ts
@@ -0,0 +1,20 @@
+import { config } from "./config.ts";
+import { OpenRouterService } from "./openrouterService.ts";
+import { createServer } from "./server.ts";
+
+const routerService = new OpenRouterService(config);
+const app = createServer(routerService);
+
+await app.listen({ port: 3000, host: "0.0.0.0" });
+console.log(`Server listening on http://localhost:3000`);
+
+/* app
+  .inject({
+    method: "POST",
+    url: "/chat",
+    body: { question: "What is the capital of Minas Gerais?" },
+  })
+  .then((response) => {
+    console.log("Response status code:", response.statusCode);
+    console.log("Response body:", response.body);
+  }); */
