b0f00cf7a352cbbcdfc5204c7d605083d20018f6
rodolfo.ensdies <rensides@hotmail.com>
2026-08-13 23:14:20 -0300

    feat/adjust-intents

--- modulo02-integracao-apis-llm/02-langchain-intro/src/graph/graph.ts
+++ modulo02-integracao-apis-llm/02-langchain-intro/src/graph/graph.ts
@@ -1,7 +1,11 @@
 import { END, MessagesZodMeta, START, StateGraph } from "@langchain/langgraph";
 import { withLangGraph } from "@langchain/langgraph/zod";
-import { BaseMessage } from "langchain";
+import { BaseMessage } from "@langchain/core/messages";
 import { z } from "zod/v3";
+import { identifyIntent } from "./nodes/identifyItentNode.ts";
+import { chatResponseNode } from "./nodes/chatResponseNode.ts";
+import { upperCaseNode } from "./nodes/upperCaseNode.ts";
+import { lowerCaseNode } from "./nodes/lowerCaseNode.ts";
 
 const GraphState = z.object({
   messages: withLangGraph(z.custom<BaseMessage[]>(), MessagesZodMeta),
@@ -15,14 +19,39 @@ export function buildGraph() {
   const workflow = new StateGraph({
     stateSchema: GraphState,
   })
-    .addNode("identifyIntent", (state: GraphState) => {
+    .addNode("identifyIntent", identifyIntent)
+    .addNode("chatResponse", chatResponseNode)
+    .addNode("uppercase", upperCaseNode)
+    .addNode("lowercase", lowerCaseNode)
+    /* .addNode("identifyIntent", (state: GraphState) => {
       return {
         ...state,
         output: "test",
       };
-    })
+    }) */
+
     .addEdge(START, "identifyIntent")
-    .addEdge("identifyIntent", END);
+    .addConditionalEdges(
+      "identifyIntent",
+      (state: GraphState) => {
+        switch (state.command) {
+          case "uppercase":
+            return "uppercase";
+          case "lowercase":
+            return "lowercase";
+          default:
+            return "chatResponse";
+        }
+      },
+      {
+        uppercase: "uppercase",
+        lowercase: "lowercase",
+      },
+    )
+    .addEdge("uppercase", "chatResponse")
+    .addEdge("lowercase", "chatResponse")
+
+    .addEdge("chatResponse", END);
 
   return workflow.compile();
 }
